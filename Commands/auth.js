43d9a988f29eaf6569934668598cc7c4a8389bd5
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2021-03-28 19:46:15 +0900

    Update auth.js

--- Commands/auth.js
+++ Commands/auth.js
@@ -1,10 +1,14 @@
-const { MessageEmbed } = require('discord.js');
+const {
+	MessageEmbed
+} = require('discord.js');
 const userModel = require('../Models/user');
 const authModel = require('../Models/auth');
 const axios = require('axios')
-const { EVENT_NAME } = require("../load")
+const {
+	EVENT_NAME
+} = require("../load")
 const instance = axios.create({
-  baseURL: process.env.BASE_API_URL
+	baseURL: process.env.BASE_API_URL
 });
 instance.defaults.headers.common['x-api-token'] = process.env.API_TOKEN
 
@@ -14,72 +18,74 @@ module.exports = {
 	disabled: false,
 	cooldown: 0,
 	ownerOnly: true,
-  guild: false,
+	guild: false,
 
 	async execute(message, args, client) {
 
-    let auth_embed = new MessageEmbed();
+		let auth_embed = new MessageEmbed();
 
-	  if(message.guild){
-	    return;
-	  }
-	  
-	  
-      var auth_info = await authModel.findOne({
-        discordId: message.author.id,
-      })
-      if(!auth_info){
-        return message.reply("`ps!register`を先に使用してください")
-      }
+		if (message.guild) {
+			return;
+		}
 
-      let NowEventId;
-      if (Date.now() < EVENT_NAME[EVENT_NAME.length - 1]["startAt"]) {
-        NowEventId = EVENT_NAME[EVENT_NAME.length - 2]["id"];
-      } else {
-        NowEventId = EVENT_NAME[EVENT_NAME.length - 1]["id"]
-      }
 
-      try {
-          var response = await instance.get(`/user/${auth_info.sekaiId}/profile`)
-          console.log(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-        
-      try{
-        var resevent = await instance.get(`/user/${auth_info.sekaiId}/event/${NowEventId}/ranking`)
-        console.log(resevent.data);
-      }catch(error){
-        console.error(error);
-      }
-	     
-      if(response.data.data.userProfile.word != auth_info.string){
-        return message.reply("認証に失敗しました。")
-      }
+		var auth_info = await authModel.findOne({
+			discordId: message.author.id,
+		})
+		if (!auth_info) {
+			return message.reply("`ps!register`を先に使用してください")
+		}
 
-	    let USER = new userModel({
-          guildId: auth_info.guildId,
-          discordId: message.author.id,
-          sekaiId: auth_info.sekaiId,
-          limit: 1,
-          sekaiProfile: response.data,
-          sekaiRanking: resevent.data
-	    })
-	    
-	    await USER.save();
-      await auth_info.deleteOne();
+		let NowEventId;
+		if (Date.now() < EVENT_NAME[EVENT_NAME.length - 1]["startAt"]) {
+			NowEventId = EVENT_NAME[EVENT_NAME.length - 2]["id"];
+		} else {
+			NowEventId = EVENT_NAME[EVENT_NAME.length - 1]["id"]
+		}
 
-      auth_embed.setAuthor(message.author.tag,message.author.displayAvatarURL)
-      .setTitle("プロセカユーザー名:"+response.data.data.user.userGamedata.name)
-      .setDescription(`ひと言:${response.data.data.userProfile.word}\n
+		try {
+			var response = await instance.get(`/user/${auth_info.sekaiId}/profile`)
+			console.log(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+
+		try {
+			var resevent = await instance.get(`/user/${auth_info.sekaiId}/event/${NowEventId}/ranking`)
+			console.log(resevent.data);
+		} catch (error) {
+			console.error(error);
+		}
+
+		if (response.data.data.userProfile.word != auth_info.string) {
+			return message.reply("認証に失敗しました。")
+		}
+
+		let USER = new userModel({
+			guildId: auth_info.guildId,
+			discordId: message.author.id,
+			sekaiId: auth_info.sekaiId,
+			limit: 1,
+			sekaiProfile: response.data,
+			sekaiRanking: resevent.data
+		})
+
+		await USER.save();
+		await auth_info.deleteOne();
+
+		auth_embed.setAuthor(message.author.tag, message.author.displayAvatarURL)
+			.setTitle("プロセカユーザー名:" + response.data.data.user.userGamedata.name)
+			.setDescription(
+				`ひと言:${response.data.data.userProfile.word}\n
       プレイヤーランク:${response.data.data.user.userGamedata.rank}\n
       イベントランキング:${resevent.data.data.rankings[0].rank}位\n
-      イベントポイント:${resevent.data.data.rankings[0].score}ポイント`)
-      .setFooter("こちらの内容で登録しました。")
-      .setTimestamp();
+      イベントポイント:${resevent.data.data.rankings[0].score}ポイント`
+			)
+			.setFooter("こちらの内容で登録しました。")
+			.setTimestamp();
 
-      
-      message.reply(auth_embed)
-	  
+
+		message.reply(auth_embed)
+
 	}
 };
